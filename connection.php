ca6c661da65c1fda3da8d0b3074d98161f356438
nhanks1412 <106572190+nhanks1412@users.noreply.github.com>
2024-04-03 13:15:27 +0700

    commit

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'vctb056kl6ur28m5','d63k9sngcivzysdl', 't90le85dom2d1eds')
+$conn = mysqli_connect('mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'hn32f7si0l771w98','t5nwr5rq8573e67s', 'vziqhuhdb38bu6o4')
         or die("Can not connect database".mysqli_connect_error());
 ?>
